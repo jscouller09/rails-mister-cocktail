eb3600ebbd74c0ab1592391283389feeef46333a
James Scouller <jscouller09@gmail.com>
2020-02-21 10:46:18 -0500

    Fixed validation error on pic_url

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,6 +3,7 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :pic_url,
+            allow_blank: true,
             format: { with: URI::regexp,
                       message: "Valid URL required" }
 end
